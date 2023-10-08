29e50e0a183ee9e22e048a700d90065284b347b7
paul <paulentz@gmail.com>
2023-10-07 19:18:31 -0500

    regex

--- Ch04/04_04/begin/app.py
+++ Ch04/04_04/begin/app.py
@@ -1,5 +1,5 @@
 miles = input('Enter a distance in miles: ')
-miles_value = float(miles)
+miles_value = int(miles)
 kilometers_value = miles_value * 1.609344
 
-print(f"The number you entered in kilometers is {kilometers_value}")
+print(f"The number you entered in kilometers is {kilometers_value} ")
--- Ch04/04_05/begin/app.py
+++ Ch04/04_05/begin/app.py
@@ -1,2 +1,6 @@
 miles = input('Enter a distance in miles: ')
 # kilometers_value = miles_value * 1.609344
+
+milesint = float(miles)
+kilometers_value = milesint * 1.609344
+print(f"Kilos is {kilometers_value}")
