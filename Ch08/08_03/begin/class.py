a4ffce84853a1bf32c3b4969bb7e1ba30a088b45
paul <paulentz@gmail.com>
2023-10-08 12:25:55 -0500

    python class practice

--- Ch08/08_03/begin/class.py
+++ Ch08/08_03/begin/class.py
@@ -1,19 +1,22 @@
 class Attendee:
     'Common base class for all attendees'
 
-    def __init__(self, name, tickets):
+    def __init__(self, name = "", tickets = ""):
         self.name = name
         self.tickets = tickets
 
     def displayAttendee(self):
         print('Name : {}, Tickets: {}'.format(self.name, self.tickets))
 
-    def addTicket(self):
-        self.tickets += 1
+    def addTicket(self, n=1):
+        self.tickets += n
         print('{}s tickets increased to {}'.format(self.name, self.tickets))
 
 
 attendee1 = Attendee('Paul', 15)
+attendee2 = Attendee()
 
 attendee1.displayAttendee()
 attendee1.addTicket()
+
+attendee2.displayAttendee()
